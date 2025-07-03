0dc929e4bf707bdbb912e7d3489d8ee1d74b99d7
Ashish Tulso Ramrakhiani <146379614+Ashish-Ramrakhiani@users.noreply.github.com>
2025-07-02 19:54:55 -0700

    Update check_weather.R

--- check_weather.R
+++ check_weather.R
@@ -3,5 +3,5 @@ check_weather <- function(lake_directory, configure_run_file, config_set_name){
   weather_ready <- FLAREr::check_noaa_present(lake_directory,
                                          configure_run_file,
                                          config_set_name)
-  return(weather_ready)
+  return(TRUE)
 }
