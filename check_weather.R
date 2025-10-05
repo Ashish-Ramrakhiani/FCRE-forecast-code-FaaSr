2954f10529865719f9043fff59798ba6786f1d81
Ashish Tulso Ramrakhiani <146379614+Ashish-Ramrakhiani@users.noreply.github.com>
2025-10-04 19:01:18 -0700

    Update check_weather.R

--- check_weather.R
+++ check_weather.R
@@ -1,5 +1,13 @@
 check_weather <- function(lake_directory, configure_run_file, config_set_name){
 
+  dirs <- list.dirs(".", recursive = TRUE, full.names = TRUE)
+  config_dirs <- dirs[grepl("configuration", dirs)]
+  if (length(config_dirs) > 0) {
+    repo_root <- dirname(config_dirs[1])
+    setwd(repo_root)
+    faasr_log(paste0('{"flare_config":"', repo_root, '"}'))
+  }
+
   weather_ready <- FLAREr::check_noaa_present(lake_directory,
                                          configure_run_file,
                                          config_set_name)
